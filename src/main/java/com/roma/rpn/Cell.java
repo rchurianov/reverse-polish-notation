c9abfeadeb1a98c8defb555d4aa6b1fe0efd0610
Roman Churyanov <Roman.Churyanov@redsys.ru>
2018-11-08 20:12:14 +0300

    Basic file parsing.

--- src/main/java/com/roma/rpn/Cell.java
+++ src/main/java/com/roma/rpn/Cell.java
@@ -11,8 +11,8 @@ public class Cell {
         expression = "";
     }
 
-    public Cell(String cell_expression) {
-        expression = cell_expression;
+    public Cell(String cellExpression) {
+        expression = cellExpression;
     }
 
     public String computeCell() {
@@ -21,13 +21,43 @@ public class Cell {
         // String[] rawElements = this.expression.split("\\+");
         // String[] rawOps = this.expression.split(rawElements);
 
-        String[] rawElements = this.expression.split("[\\+-\\*/]");
+        //Stack of operands
+        LinkedList<String> operands = new LinkedList<String>();
+        //Stack of operations
+        LinkedList<String> operations = new LinkedList<String>();
 
-        // LinkedList<char> ops = new LinkedList<char>();
-        // LinkedList<String> elems = new LinkedList<String>();
+        // String[] rawElements = this.expression.split("[\\+-\\*/]");
+        char[] exprArray = expression.toCharArray();
 
+        int startIndex = 0;
+        int endIndex = 0;
 
-        result = java.util.Arrays.toString(rawElements);
+        for (int i = 0; i < exprArray.length; i++) {
+            if (isOp(exprArray[i]))  {
+                // Push everything after the last operation
+                // and before the current operation to the stack
+                end_index = i;
+                operands.push(expression.substring(startIndex, endIndex));
+                startIndex = i + 1;
+
+                // Push the operation to the opesrtions stack
+                operations.push(expression.substring(i, i));
+            }
+
+            // Push the last operand to the stack
+            if (i == exprArray.length - 1) {
+                endIndex = i;
+                operands.push(expression.substring(startIndex, endIndex));
+            }
+        }
+
+        // All operands and operations are in the stacks
+
+        while (opearions.size() != 0) {
+            String op = operations.pop();
+            result = calculate(operands, op);
+        }
+        // result = java.util.Arrays.toString(rawElements);
 
         return result;
     }
@@ -36,12 +66,28 @@ public class Cell {
         return expression;
     }
 
-    public boolean is_op (char c) {
+    public boolean isOp (char c) {
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
 
-    // public String calculate(LinkedList<String> operands, LinkedList<char> operations) {
-    //
-    // }
+     public String calculate(LinkedList<String> operands, String operation) {
+        int firstArgument = Integer.parseInt(operands.pop());
+        int secondArgument = Integer.parseInt(operands.pop());
+        int result = 0;
+
+        switch (operation) {
+            case "+": result = firstArgument + secondArgument;
+                        break;
+            case "-": result = firstArgument - secondArgument;
+                        break;
+            case "*": result = firstArgument * secondArgument;
+                        break;
+            case "/": result = firstArgument / secondArgument;
+        }
+
+        operands.push(Integer.toString(result));
+
+        return Integer.toString(result);
+    }
 
 }
